f66f7e5ea6eb1915365ef5365d4510f2515085b7
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-01-01 11:05:20 +0200

    started site, finished header

---
+++ src/Header/Header.js
@@ -0,0 +1,101 @@
+import React,{Fragment, useState, useEffect} from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+function Header() {
+      // for navbar to change color on scroll
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 100);
+    });
+  }, []);
+  return (
+    <Fragment>
+      <input type="checkbox" id="check" />
+      <label htmlFor="check">
+        <div id="btn">
+          <div className="bar-holder">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </label>
+      <label htmlFor="check">
+        <div className="close-bar" id="cancel">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </label>
+      
+      <nav className={scroll ? "sidebar sidebar2" : "sidebar "}>
+      <Link className='header-link'><h1>Value Pave</h1></Link>
+      
+     
+        <ul>
+          <li className="active">
+            <Link
+              className="link"
+              to="/"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="link"
+              to="about"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+            >
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="link"
+              to="projects"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="link"
+              to="contact"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+       {/*  <div className="social-media__wrapper">
+        <i className="fab fa-instagram"></i>
+        <i className="fab fa-linkedin-in"></i>
+      </div> */}
+      </nav>
+    </Fragment>
+  );
+}
+
+export default Header;
